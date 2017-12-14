097de39968486da9208cbbc68fe6f6df6abe05d1
yamanaka <yamanaka@meme.ne.jp>
2017-12-14 13:48:46 +0900

    npm criptの並列処理を出来るように修正

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,1 +1,63 @@
-!function(e){function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}var t={};n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{configurable:!1,enumerable:!0,get:r})},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,n){return Object.prototype.hasOwnProperty.call(e,n)},n.p="",n(n.s=1)}([,function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var r=t(2);r.a.method1(),r.a.method2()},function(e,n,t){"use strict";function r(e,n){if(!(e instanceof n))throw new TypeError("Cannot call a class as a function")}var o=function(){function e(e,n){for(var t=0;t<n.length;t++){var r=n[t];r.enumerable=r.enumerable||!1,r.configurable=!0,"value"in r&&(r.writable=!0),Object.defineProperty(e,r.key,r)}}return function(n,t,r){return t&&e(n.prototype,t),r&&e(n,r),n}}(),u=function(){function e(){r(this,e)}return o(e,null,[{key:"method1",value:function(){}},{key:"method2",value:function(){}}]),e}();n.a=u}]);
+webpackJsonp([0],[
+/* 0 */,
+/* 1 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__sub__ = __webpack_require__(2);
+
+
+__WEBPACK_IMPORTED_MODULE_0__sub__["a" /* default */].method1();
+__WEBPACK_IMPORTED_MODULE_0__sub__["a" /* default */].method2();
+
+timer(1000);
+
+function timer(delay) {
+	var myFirstPromise = new Promise(function (resolve, reject) {
+		setTimeout(function () {
+			resolve("Success!"); // Yay! Everything went well!
+		}, delay);
+	});
+
+	myFirstPromise.then(function (successMessage) {
+		// successMessage is whatever we passed in the resolve(...) function above.
+		// It doesn't have to be a string, but if it is only a succeed message, it probably will be.
+		console.log("Yay! " + successMessage);
+	});
+}
+
+/***/ }),
+/* 2 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Utils = function () {
+	function Utils() {
+		_classCallCheck(this, Utils);
+	}
+
+	_createClass(Utils, null, [{
+		key: 'method1',
+		value: function method1() {
+			console.log('method1');
+		}
+	}, {
+		key: 'method2',
+		value: function method2() {
+			console.log('method2');
+		}
+	}]);
+
+	return Utils;
+}();
+
+/* harmony default export */ __webpack_exports__["a"] = (Utils);
+
+/***/ })
+],[1]);
+//# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,19 @@ import Utils from './_sub';
 Utils.method1();
 Utils.method2();
 
-console.log('aaaaa');
-console.log($);
-console.log('ccccc');
+timer(1000);
+
+
+function timer(delay) {
+	let myFirstPromise = new Promise((resolve, reject) => {
+		setTimeout(function(){
+			resolve("Success!"); // Yay! Everything went well!
+		}, delay);
+	});
+
+	myFirstPromise.then((successMessage) => {
+		// successMessage is whatever we passed in the resolve(...) function above.
+		// It doesn't have to be a string, but if it is only a succeed message, it probably will be.
+		console.log("Yay! " + successMessage);
+	});
+}
